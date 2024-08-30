5b4d1477cdee3b00bef86d36e3555581662a0781
alphatheassistant <92854101+alphatheassistant@users.noreply.github.com>
2024-08-30 09:46:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version="0.1",
 description="A python api to use perplexity.ai",
 long_description=long_description,
 long_description_content_type="text/markdown",
-url="https://github.com/nathanrchn/perplexityai",
+url="https://github.com/alphatheassistant/perplexityai",
 packages=find_packages(),
 install_requires=["requests", "websocket-client"]
 )
